19be0fde00aad5be54dc219c19eecb812dc03b55
stephendedalus46 <cieciur@onet.pl>
2020-07-29 14:54:56 +0200

    first request

--- youtube.py
+++ youtube.py
@@ -1,3 +1,15 @@
+import os
+from googleapiclient.discovery import build
 
+api_key = os.environ.get('YT_API_KEY')
 
-api_key = 'AIzaSyCebixDGd4hUm2KS7rGwsMz1hCDxt-rOE0'
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+request = youtube.channels().list(
+    part='statistics',
+    forUsername='schafer5'
+)
+
+response = request.execute()
+
+print(response)
